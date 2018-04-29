c3aca726588b677a9719b4fc5354f1bde4d163de
Karuna Veer <karunanithi@tpconnects.com>
2018-04-29 13:08:26 +0400

    First commit

---
+++ JTAppleCalendarDemo/Cells/CalendarHeader.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarHeader.swift
+//  JTAppleCalendarDemo
+//
+//  Created by Yasheed Muhammed on 4/26/18.
+//  Copyright © 2018 Yasheed Muhammed. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+class CalendarHeader: JTAppleCollectionReusableView {
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var yearLabel: UILabel!
+}
---
+++ JTAppleCalendarDemo/Cells/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  JTAppleCalendarDemo
+//
+//  Created by Yasheed Muhammed on 4/26/18.
+//  Copyright © 2018 Yasheed Muhammed. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+class CustomCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var selectedView: UIView!
+    @IBOutlet weak var rightView: UIView!
+    @IBOutlet weak var leftView: UIView!
+
+}
